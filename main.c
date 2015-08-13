67066a710252ac241aef420c563e53173d62ee9c
mcerdeiro <mcerdeiro@gmail.com>
2015-08-13 15:58:00 +0200

    this is an error

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 
-
 int main (void)
 {
+   if (condition)
+   {
       printf("This is my first git example.\n");
 
       printf("This is my first git example 2.\n");
+
+   }
       return 0;
 }
